3807d3446ef85ec9f725e2dc7ecbe427a2cb89ff
omar <el.ell.elassani@gmail.com>
2023-05-10 23:43:33 +0100

    changing the local host address

--- lib/core/constants/link_api.dart
+++ lib/core/constants/link_api.dart
@@ -1,15 +1,14 @@
-class AppLink { 
+class AppLink {
   // base url
-  static const String serverLink = "http://10.0.2.2/projects/carentalApiPhp"; 
-  // auth urls 
-  static const String signUpLink = "$serverLink/auth/signup.php";  
-  static const String loginLink = "$serverLink/auth/login.php";   
-  // image url 
+  static const String serverLink = "http://10.0.2.2/projects/carentalApiPhp";
+  // auth urls
+  static const String signUpLink = "$serverLink/auth/signup.php";
+  static const String loginLink = "$serverLink/auth/login.php";
+  // image url
   static const String imageLink = "$serverLink/uploads/";
-  // static const String test = "$serverLink/test.php";  
+  // static const String test = "$serverLink/test.php";
   static const String favoritlink = "$serverLink/favorit.php";
-  
-  // home urls 
-  static const String carsLink = "$serverLink/carCart/car_cart.php";
 
+  // home urls
+  static const String carsLink = "$serverLink/carCart/car_cart.php";
 }

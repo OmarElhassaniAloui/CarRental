3157cd952a9d638aa05dc0557dc638f394d4e02b
omar <el.ell.elassani@gmail.com>
2023-06-21 19:19:28 +0100

    creating confirmation controller

---
+++ lib/controller/reservation_controllers/confiramtion_controller.dart
@@ -0,0 +1,17 @@
+import 'package:get/get.dart'; 
+
+class ConfiramtionController extends GetxController { 
+  
+
+
+
+
+
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  @override
+  void onClose() {}
+}
